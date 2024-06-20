a0ac2b3ca9e5efb901eab65b130f6a28557fb5d2
hyun907 <seunghyun020907@naver.com>
2024-06-20 22:28:56 +0900

    Feat: Home 페이지 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -10,19 +10,19 @@ import Notfound from "./pages/Notfound";
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-06-20").getTime(),
     emotionId: 1,
     content: "1번 일기 내용",
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-06-19").getTime(),
     emotionId: 2,
     content: "2번 일기 내용",
   },
   {
     id: 3,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2024-05-19").getTime(),
     emotionId: 3,
     content: "3번 일기 내용",
   },
@@ -43,8 +43,8 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
