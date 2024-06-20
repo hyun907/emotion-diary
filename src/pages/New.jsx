9448c9dd158948964a2102eb1d7bcd4e35cc9929
hyun907 <seunghyun020907@naver.com>
2024-06-21 06:40:47 +0900

    Feat: New 페이지 날짜 선택, 감정 선택, 일기 작성 기능 구현

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -2,14 +2,28 @@ import React from "react";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+import Diary from "./Diary";
 
 const New = () => {
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const nav = useNavigate();
+
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+
+    nav("/", { replace: true });
+  };
+
   return (
     <div>
-      <Header title={"새 일기 쓰기"} 
-      leftChild={<Button text={"< 뒤로가기"}/>}
+      <Header
+        title={"새 일기 쓰기"}
+        leftChild={<Button onClick={() => nav(-1)} text={"< 뒤로가기"} />}
       />
-      <Editor/>
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
