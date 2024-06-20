d75cc4c936b2f352c699af8730a1959d12cdae2c
hyun907 <seunghyun020907@naver.com>
2024-06-21 04:34:34 +0900

    Feat: New 페이지 UI 구현

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
 
 const New = () => {
-  return <div>new</div>;
+  return (
+    <div>
+      <Header title={"새 일기 쓰기"} 
+      leftChild={<Button text={"< 뒤로가기"}/>}
+      />
+      <Editor/>
+    </div>
+  );
 };
 
 export default New;
